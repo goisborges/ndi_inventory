0657afca1ee64afb5c38dea9548f2403be44fec3
goisborges <goisborges@gmail.com>
2024-06-13 16:29:38 -0400

    added location model. Changed admin UI on admin.py and models.py

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
+from django.db import models
 
 # Register your models here.
+
+from .models import *
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'quantity', 'location', 'category')
+    list_filter = ('name','category')
+
+admin.site.register(Product, ProductAdmin) 
+admin.site.register(Category)
+admin.site.register(Location)
+
+#change the admin site header
+admin.site.site_header = 'NDI Inventory Admin Site'
+
+
--- dashboard/models.py
+++ dashboard/models.py
@@ -1,17 +1,32 @@
+from os import name
 from unicodedata import category
 from django.db import models
 
+
+
 # Create your models here.
 
-class Products(models.Model):
+class Product(models.Model):
     name = models.CharField(max_length = 50, null = True)
-    category = models.ForeignKey('Category', on_delete = models.CASCADE)
-    quantity = models.IntegerField()
+    category = models.ForeignKey('Category', on_delete = models.CASCADE, null = True)
+    quantity = models.IntegerField(null = True)
     sku_number = models.CharField(max_length = 50, unique = True)
+    location = models.ForeignKey('Location', on_delete = models.CASCADE, null = True)
     description = models.TextField()
-    photo_image = models.ImageField(upload_to='dashboard/media')
+    photo_image = models.ImageField(upload_to='dashboard/media', null=True, blank = True)
 
+    def __str__(self):
+        return f'{self.name} / {self.quantity}'
+
+class  Location(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
+
+    def __str__(self):
+        return self.name
