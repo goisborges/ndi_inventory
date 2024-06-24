f20b133c582a9674e60c5f2f28a9ec0686b32375
goisborges <goisborges@gmail.com>
2024-06-24 16:36:42 -0400

    adding login and new register form

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import login
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('staff/', views.staff, name='dashboard-staff'),
     path('', views.index, name='dashboard-index' ),
     path('products/', views.products, name='dashboard-products' ),
     path('inventory/', views.inventory, name='dashboard-inventory' ),
-    path('categories/', views.categories, name='dashboard-categories')
+    path('categories/', views.categories, name='dashboard-categories'),
+    path('login', auth_views.LoginView.as_view(template_name = 'user/login.html'), name = 'user-login'),
+    path('logout', auth_views.LogoutView.as_view(template_name = 'user/logout.html') , name = 'user-logout')
 ]
--- user/views.py
+++ user/views.py
@@ -1,20 +1,20 @@
-from operator import is_
-from pyexpat.errors import messages
+
 from django.shortcuts import redirect, render
 from django.contrib.auth.forms import UserCreationForm
+from .forms import CreateUserForm
 
 # Create your views here.
 
 def register(request):
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
 
             # Redirect to dashboard after user creation
             return redirect('dashboard-index')
     else:
-        form = UserCreationForm()
+        form = CreateUserForm()
     context = {
         'form': form
 
